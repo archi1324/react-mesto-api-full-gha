8699331c62758c75973af766a71cb5943db1bcb7
Артур Саяхов <artursaahov@MacBook-Air-Artur.local>
2023-10-05 15:23:05 +0500

    15pr final after review

--- backend/controllers/card.js
+++ backend/controllers/card.js
@@ -30,7 +30,7 @@ module.exports.deleteCard = (req, res, next) => {
         throw new NotFound('Карточка по id не найдена');
       }
       if (card.owner._id.valueOf() !== req.user._id) {
-        next(new Forbidden('Ошибка прав доступа'));
+        throw new Forbidden('Ошибка прав доступа');
       }
       return Card.deleteOne(card)
         .then(() => res.send(card));
